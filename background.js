b7e5aa59fa93e4766de4741426c2315e8b0001ab
Bohdan <dogdanlashko@gmail.com>
2023-07-23 21:10:39 +0200

    refactoring

--- background.js
+++ background.js
@@ -1,62 +1,42 @@
-// import getRules from './rules';
+const allResourceTypes = Object.values(chrome.declarativeNetRequest.ResourceType)
 
-// alert('hello world');
-
-console.log('hello world');
-
-const allResourceTypes = Object.values(chrome.declarativeNetRequest.ResourceType);
-
-function getRules(token) {
-    console.log('getRules', token)
-    const headerValue = 'Bearer ' + token;
-    const rules = [
-        {
-            id: 1,
-            priority: 1,
-            action: {
-                type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
-                requestHeaders: [
-                    {
-                        operation: chrome.declarativeNetRequest.HeaderOperation.SET,
-                        header: 'Authorization',
-                        value: headerValue,
-                    },
-                ],
-            },
-            condition: {
-                urlFilter: '*',
-                resourceTypes: allResourceTypes,
-            },
+const getRules = (token) => {
+    const headerValue = 'Bearer ' + token
+    const headerRule = {
+        id: 1,
+        priority: 1,
+        action: {
+            type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+            requestHeaders: [
+                {
+                    operation: chrome.declarativeNetRequest.HeaderOperation.SET,
+                    header: 'Authorization',
+                    value: headerValue,
+                },
+            ],
         },
-    ];
-    return rules;
+        condition: {
+            urlFilter: '*',
+            resourceTypes: allResourceTypes,
+        },
+    }
+    return [headerRule]
 }
 
 function updateHeaders(token) {
-    console.log('updateHeaders', token)
-    const rules = getRules(token);
+    const rules = getRules(token)
     chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: rules.map((rule) => rule.id), // remove existing rules
-        addRules: rules, // add new rules
-    });
+        removeRuleIds: rules.map((rule) => rule.id),
+        addRules: rules,
+    })
 }
 
 chrome.storage.local.get('access_token', (items) => {
-    const accessToken = items.access_token;
-    if (accessToken) {
-        updateHeaders(accessToken);
-    }
-});
+    const accessToken = items.access_token
+    if (accessToken) updateHeaders(accessToken)
+})
 
 chrome.storage.local.onChanged.addListener((changes) => {
-    console.log('storage.onChanged')
-    console.log(changes)
-    if (changes.access_token) {
-        updateHeaders(changes.access_token.newValue);
-    }
-});
-
-// chrome.action.onClicked.addListener((tab) => {
-//     console.log('action.onClicked')
-//     chrome.storage.local.set({ access_token: 'a', refresh_token: 'b' }, () => { })
-// });
+    if (changes.access_token)
+        updateHeaders(changes.access_token.newValue)
+})
--- script.js
+++ script.js
@@ -1,90 +1,47 @@
-console.log('hello js');
-
 const refreshUrlInput = document.getElementById('refresh_url_input');
-const accessTokenInput = document.getElementById('access_token_input');
-const refreshTokenInput = document.getElementById('refresh_token_input');
-const refreshButton = document.getElementById('refresh_button');
-
-// chrome.tabs.getSelected(null, function (tab) {
-//     console.log(tab)
-//     console.log(tab.url)
-//     console.log(tab.host)
-//     setRefreshUrlKey(tab.host);
-//     loadRefreshUrl();
-// });
+const accessTokenInput = document.getElementById('access_token_input')
+const refreshTokenInput = document.getElementById('refresh_token_input')
+const refreshButton = document.getElementById('refresh_button')
 
 chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-    const tab = tabs[0];
-    console.log(tab)
-    console.log(tab.url)
-    const host = tab.url.split('/')[2];
-    console.log(host)
+    const tab = tabs[0]
+    const host = tab.url.split('/')[2]
 
-    const refreshUrlKey = 'refresh_url__' + host;
-
-    console.log(refreshUrlKey);
+    const refreshUrlKey = 'refresh_url__' + host
 
     chrome.storage.local.get(refreshUrlKey, (items) => {
-        console.log(items)
         refreshUrlInput.value = items[refreshUrlKey] || '';
-    });
+    })
 
     refreshUrlInput.onchange = () =>
-        chrome.storage.local.set({ [refreshUrlKey]: refreshUrlInput.value });
-
-    // refreshUrlInput.onchange = (e) => {
-    //     console.log(e)
-    //     chrome.storage.local.set({ [refreshUrlKey]: refreshUrlInput.value })
-    // };
-});
+        chrome.storage.local.set({ [refreshUrlKey]: refreshUrlInput.value })
+})
 
 chrome.storage.local.get(
     ['access_token', 'refresh_token'],
     (items) => {
-        accessTokenInput.value = items.access_token || '';
-        refreshTokenInput.value = items.refresh_token || '';
+        accessTokenInput.value = items.access_token || ''
+        refreshTokenInput.value = items.refresh_token || ''
     },
-);
+)
 
 accessTokenInput.onchange = () =>
-    chrome.storage.local.set({ 'access_token': accessTokenInput.value });
+    chrome.storage.local.set({ 'access_token': accessTokenInput.value })
 refreshTokenInput.onchange = () =>
-    chrome.storage.local.set({ 'refresh_token': refreshTokenInput.value });
+    chrome.storage.local.set({ 'refresh_token': refreshTokenInput.value })
 
 
 refreshButton.onclick = async () => {
-    console.log('Refresh')
-    const url = refreshUrlInput.value;
-    // window.location.toString()
+    const url = refreshUrlInput.value
     const response = await fetch(url, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ 'refresh': refreshTokenInput.value }),
-    });
-    const data = await response.json();
-    console.log(data);
-    const accessToken = data.access;
+    })
+    const data = await response.json()
+    const accessToken = data.access
     if (accessToken) {
-        accessTokenInput.value = data.access;
+        accessTokenInput.value = data.access
         chrome.storage.local.set({ 'access_token': data.access })
     }
 }
-
-
-
-// chrome.storage.local.get('access_token', (items) => {
-//     accessTokenInput.value = items.access_token
-// })
-
-// chrome.storage.local.get('refresh_token', (items) => {
-//     refreshTokenInput.value = items.refresh_token
-// })
-
-// const loadData = async () => {
-//     console.log(await chrome.storage.local.get(['access_token', 'refresh_token']))
-
-//     // accessTokenInput.value = await chrome.storage.local.get('access_token');
-//     // refreshTokenInput.value = await chrome.storage.local.get('refresh_token');
-// }
-
-// loadData();
