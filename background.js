663027997674533348a5cc00e31f61b55eb76e9e
Bohdan <dogdanlashko@gmail.com>
2023-07-23 21:41:39 +0200

    enable button

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 const allResourceTypes = Object.values(chrome.declarativeNetRequest.ResourceType)
 
 const getRules = (token) => {
-    const headerValue = 'Bearer ' + token
+    const headerValue = token ? 'Bearer ' + token : ''
     const headerRule = {
         id: 1,
         priority: 1,
@@ -23,7 +23,7 @@ const getRules = (token) => {
     return [headerRule]
 }
 
-function updateHeaders(token) {
+const updateHeaders = (token) => {
     const rules = getRules(token)
     chrome.declarativeNetRequest.updateDynamicRules({
         removeRuleIds: rules.map((rule) => rule.id),
@@ -31,12 +31,31 @@ function updateHeaders(token) {
     })
 }
 
-chrome.storage.local.get('access_token', (items) => {
-    const accessToken = items.access_token
-    if (accessToken) updateHeaders(accessToken)
-})
+const removeHeaders = () => {
+    console.log
+    const rules = getRules()
+    chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: rules.map((rule) => rule.id),
+        addRules: [],
+    })
+}
+
+const addHeaders = () => {
+    chrome.storage.local.get('access_token', (items) => {
+        const accessToken = items.access_token
+        if (accessToken) updateHeaders(accessToken)
+    })
+}
+
+addHeaders()
 
 chrome.storage.local.onChanged.addListener((changes) => {
     if (changes.access_token)
         updateHeaders(changes.access_token.newValue)
+
+    if (changes.enabled) {
+        console.log(changes.enabled)
+        if (changes.enabled.newValue) addHeaders()
+        else removeHeaders()
+    }
 })
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const enabledInput = document.getElementById('enabled_checkbox');
 const refreshUrlInput = document.getElementById('refresh_url_input');
 const accessTokenInput = document.getElementById('access_token_input')
 const refreshTokenInput = document.getElementById('refresh_token_input')
@@ -10,20 +11,34 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
     const refreshUrlKey = 'refresh_url__' + host
 
     chrome.storage.local.get(refreshUrlKey, (items) => {
-        refreshUrlInput.value = items[refreshUrlKey] || '';
+        refreshUrlInput.value = items[refreshUrlKey] || ''
     })
 
     refreshUrlInput.onchange = () =>
         chrome.storage.local.set({ [refreshUrlKey]: refreshUrlInput.value })
+
+    // const enabledKey = 'enabled__' + host
+
+    // chrome.storage.local.get(enabledKey, (items) => {
+    //     enabledInput.value = items[enabledKey] || ''
+    // })
+
+    // enabledInput.onchange = () =>
+    //     chrome.storage.local.set({ [enabledKey]: enabledInput.value })
 })
 
 chrome.storage.local.get(
-    ['access_token', 'refresh_token'],
+    ['access_token', 'refresh_token', 'enabled'],
     (items) => {
+        enabledInput.checked = items.enabled || false
+
         accessTokenInput.value = items.access_token || ''
         refreshTokenInput.value = items.refresh_token || ''
     },
 )
+
+enabledInput.onchange = () =>
+    chrome.storage.local.set({ 'enabled': enabledInput.checked })
 
 accessTokenInput.onchange = () =>
     chrome.storage.local.set({ 'access_token': accessTokenInput.value })
